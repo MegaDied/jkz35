6b4a6a26c1ba780d7738de99ad5516a60661103d
klaas0 <37308015+klaas0@users.noreply.github.com>
2018-03-22 22:52:00 +0100

    Update dbconnect.php

--- functions/dbconnect.php
+++ functions/dbconnect.php
@@ -7,4 +7,5 @@
 
 	$connection = mysqli_connect($sqlserver, $sqluser, $sqlpass, $sqldb) or die ("Can't connect to the Database. Please try again later.");
 	# Maak een connectie met de database of stop en laat het bericht "Can't connect to DB" zien
+date_default_timezone_set('Europe/Amsterdam');
 ?>
